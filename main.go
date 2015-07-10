dc154de722651c74374d0c07587dd4aa60999d5d
Hendra Uzia <hendra.uzia@gmail.com>
2015-07-10 13:23:44 +0700

    Add concurrency on multiple API queries

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 func main() {
   mw := multiWeatherProvider{
     openWeatherMap{},
-    weatherUnderground{apiKey: "your-key-here"},
+    weatherUnderground{apiKey: "your-api-key"},
   }
 
   http.HandleFunc("/hello", hello)
@@ -120,15 +120,29 @@ func temperature(city string, providers ...weatherProvider) (float64, error) {
 type multiWeatherProvider []weatherProvider
 
 func (w multiWeatherProvider) temperature(city string) (float64, error) {
-  sum := 0.0
+  temps := make(chan float64, len(w))
+  errs  := make(chan error,   len(w))
 
   for _, provider := range w {
-    k, err := provider.temperature(city)
-    if err != nil {
-      return 0, err
-    }
+    go func(p weatherProvider) {
+      k, err := provider.temperature(city)
+      if err != nil {
+        errs <- err
+        return
+      }
 
-    sum += k
+      temps <- k
+    }(provider)
+  }
+
+  sum := 0.0
+  for i := 0; i < len(w); i++ {
+    select {
+      case temp := <-temps:
+        sum += temp
+      case err := <-errs:
+        return 0, err
+    }
   }
 
   return sum / float64(len(w)), nil
